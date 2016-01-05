7470fb57cbf9ad9a513aa62d3a26288bf3b44919
Savvas <savvas.tjortjoglou.com>
2016-01-05 00:19:32 -0500

    changed setup.py got rid of wheel and setup.cfg

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.0a1',
+    version='0.1.0a1',
 
     description='A visualization library that helps create NBA player shot charts.',
     long_description=long_description,
